d6f1b4aa9124be8549fa175ca1ecb8f8122431ca
vily <vily@ipu.dk>
2024-05-07 12:54:04 +0200

    Removed auto node tooltip

--- NodeGraphQt/qgraphics/node_base.py
+++ NodeGraphQt/qgraphics/node_base.py
@@ -279,7 +279,7 @@ class NodeItem(AbstractNodeItem):
         if state:
             tooltip += ' <font color="red"><b>(DISABLED)</b></font>'
         tooltip += '<br/>{}<br/>'.format(self.type_)
-        self.setToolTip(tooltip)
+        # self.setToolTip(tooltip)
 
     def _set_base_size(self, add_w=0.0, add_h=0.0):
         """
